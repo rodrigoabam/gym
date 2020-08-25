07e66029b5050da2e041aeb861615088a63e21dd
rodrigoabam <rodrigoab.am@gmail.com>
2020-08-25 13:57:39 -0400

    funcionalidade deletar e editar

--- routes.js
+++ routes.js
@@ -2,26 +2,30 @@ const express = require('express')
 const instructors = require("./instructors")
 const routes = express.Router()
 
-routes.get('/', function(req, res){
-    return res.redirect('/instructors')
+routes.get("/", function(req, res){
+    return res.redirect("/instructors")
 })
 
-routes.get('/instructors', function(req, res){
-    return res.render('instructors/index')
+routes.get("/instructors", function(req, res){
+    return res.render("instructors/index")
 })
 
-routes.get('/instructors/create', function(req, res){
-    return res.render('instructors/create')
+routes.get("/instructors/create", function(req, res){
+    return res.render("instructors/create")
 })
 
-routes.get('/instructors/:id', instructors.show)
+routes.get("/instructors/:id", instructors.show)
 
-routes.get('/instructors/:id/edit', instructors.edit)
+routes.get("/instructors/:id/edit", instructors.edit)
 
 routes.post("/instructors", instructors.post)
 
-routes.get('/members', function(req, res){
-    return res.send('members')
+routes.put("/instructors", instructors.put)
+
+routes.delete("/instructors", instructors.delete)
+
+routes.get("/members", function(req, res){
+    return res.send("members")
 })
 
 module.exports = routes;
